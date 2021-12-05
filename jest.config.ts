20acd96dadd6f588a5f14286e273cd70dbfc0305
JIANXUAN LI <liujin834@gmail.com>
2021-12-05 21:02:07 +0800

    initialized express app by typescript

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  moduleFileExtensions: ["js", "json", "ts"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+};
+
+export default config;
